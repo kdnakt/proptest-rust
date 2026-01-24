3a576e96f1300177c092d162f1921550f5375fbe
kdnakt <a.kid.1985@gmail.com>
2026-01-25 00:08:24 +0900

    test_utils::vec()

--- src/test_utils/proptest_strategies.rs
+++ src/test_utils/proptest_strategies.rs
@@ -1,6 +1,14 @@
 use proptest::prelude::*;
+use proptest::collection;
 use uuid::Uuid;
 
 pub(crate) fn uuid() -> impl Strategy<Value = Uuid> {
     (any::<u128>()).prop_map(|num| Uuid::from_u128(num))
+}
+
+pub(crate) fn vec<T>() -> impl Strategy<Value = Vec<T>>
+where
+    T: Arbitrary,
+{
+    collection::vec(any::<T>(), collection::size_range(0..10))
 }
